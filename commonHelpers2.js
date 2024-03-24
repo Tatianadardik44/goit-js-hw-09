a7f737d5225085f3661d9fc6b9dba4a82b8f045e
Tatianadardik44 <149703862+Tatianadardik44@users.noreply.github.com>
2024-03-24 17:27:02 +0000

    Deploying to gh-pages from @ Tatianadardik44/goit-js-hw-09@f082299a2e403ba5e20135207b658af2f8e6f125 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/styles-6210291f.js";console.log("Form");
+import"./assets/styles-789314c2.js";const e=document.querySelector("form"),s="feedback-form-state";e.addEventListener("input",l);e.addEventListener("submit",n);function l(t){t.preventDefault();const a={email:e.email.value.trim(),message:e.message.value.trim()};localStorage.setItem(s,JSON.stringify(a))}function m(){const t=localStorage.getItem(s),a=JSON.parse(t);a&&(e.elements.email.value=a.email,e.elements.message.value=a.message)}m();function n(t){t.preventDefault(),e.elements.email.value&&e.elements.message.value&&(console.log(localStorage.getItem(s)),t.currentTarget.reset(),localStorage.removeItem(s))}
 //# sourceMappingURL=commonHelpers2.js.map
